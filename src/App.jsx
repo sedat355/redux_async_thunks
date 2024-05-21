70dcab050e0bcab8161f153f09b5842faf1a2457
Sedat Polat <sedatpolat355@gmail.com>
2024-05-21 12:41:10 +0300

    store ve slice oluşturuldu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import UsersList from "./components/UsersList"
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="">
+      <UsersList/>
+    </div>
   )
 }
 
