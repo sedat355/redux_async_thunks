627d7e16e89048406282d884a947bba07d69ac6a
Sedat Polat <sedatpolat355@gmail.com>
2024-05-22 07:44:55 +0300

    user list render edildi

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchUsers } from '../store/thunks/fetchUsers';
 import Skeleton from './Skeleton';
+import { render } from 'react-dom';
 
 const UsersList = () => {
   const dispatch = useDispatch();
@@ -19,10 +20,23 @@ const UsersList = () => {
     return <div>Veri getirmede bir hata oluştu</div>
   }
 
+  const renderedUsers = data.map(user => {
+    return(
+      <div 
+        key={user.id}
+        className='mb-2 border rounded'  
+      >
+        <div className='flex p-2 justify-between items-center cursor-pointer'>
+          {user.name}
+        </div>
+      </div>
+    )
+  })
+
   return (
-    <ul>
-      userlist
-    </ul>
+    <>
+      {renderedUsers}
+    </>
   )
 }
 
