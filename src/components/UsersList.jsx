4195b97a6263f00104878714a7ea7dffffd7b4c3
Sedat Polat <sedatpolat355@gmail.com>
2024-05-23 11:07:04 +0300

    UserList' in return' ü yeniden düzenlendi

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -25,36 +25,35 @@ const UsersList = () => {
     doCreateUser();
   }
 
-  if(isLoadingUsers) {
-    return <Skeleton times={4} className="h-10 w-full"/>
-  }
-
-  if(loadingUsersError) {
-    return <div>Veri getirmede bir hata oluştu</div>
-  }
-
-  const renderedUsers = data.map(user => {
-    return(
-      <div 
-        key={user.id}
-        className='mb-2 border rounded'  
-      >
-        <div className='flex p-2 justify-between items-center cursor-pointer'>
-          {user.name}
+  let content;
+  if( isLoadingUsers ) {
+    content = <Skeleton times={4} className="h-10 w-full"/>
+  } else if( loadingUsersError ) {
+    content = <div>Veri getirmede bir hata oluştu</div>
+  } else {
+    content = data.map(user => {
+      return(
+        <div 
+          key={user.id}
+          className='mb-2 border rounded'  
+        >
+          <div className='flex p-2 justify-between items-center cursor-pointer'>
+            {user.name}
+          </div>
         </div>
-      </div>
-    )
-  })
+      )
+    })
+  }
 
   return (
     <div>
-      <div className='flex flex-row justify-between m-3'>
+      <div className='flex flex-row justify-between items-center m-3'>
         <h1 className='m-2 text-xl'>Users</h1>
         <Button loading={isCreatingUser} onClick={handleUserAdd}>+Add User</Button>
         { creatingUserError && 'Error creating user!!'}
       </div>
 
-      {renderedUsers}
+      {content}
     </div>
   )
 }
