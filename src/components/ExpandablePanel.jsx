209eb4ccaa5d052d11769c441ecf7ec5d29d7807
Sedat Polat <sedatpolat355@gmail.com>
2024-05-23 14:40:44 +0300

    user daki panel açma kapama çalışıyor

--- src/components/ExpandablePanel.jsx
+++ src/components/ExpandablePanel.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { GoChevronDown, GoChevronLeft } from 'react-icons/go'
 
 const ExpandablePanel = ({header, children}) => {
+  const [ expanded, setExpanded ] = useState(false)
+
   return (
     <div className="mb-2 border rounded">
-      <div className="flex gap-x-2 p-2 justify-between items-center cursor-pointer">
+      <div className="flex gap-x-2 p-2 justify-between items-center ">
         {header}
+        <div onClick={() => setExpanded(!expanded)} className='cursor-pointer'>
+          {expanded ? <GoChevronDown/> : <GoChevronLeft/>}
+        </div>
       </div>
-      <div className="p-2 border-t">{children}</div>
+      { expanded && <div className="p-2 border-t">{children}</div> }
     </div>
   )
 }
