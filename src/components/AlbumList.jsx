5b8d94a1a5bc057e8b922118e83e11eaa34bf224
Sedat Polat <sedatpolat355@gmail.com>
2024-05-25 15:45:17 +0300

    Album listesi render edildi

--- src/components/AlbumList.jsx
+++ src/components/AlbumList.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import { useFetchAlbumsQuery } from '../store'
+import Skeleton from './Skeleton';
+import ExpandablePanel from './ExpandablePanel';
 
 const AlbumList = ({user}) => {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
-
-  console.log(data, error, isLoading)
   
+  let content;
+  if( isLoading ) {
+    content = <Skeleton times={3}/>
+  } else if( error ) {
+    content = <div>Error loading albums!!</div>
+  } else {
+    content = data.map(album => {
+      const header = <div>{album.title}</div>
+
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of photos in the album
+        </ExpandablePanel>
+      )
+    })
+  }
+
   return (
-    <div>Albums for {user.name}</div>
+    <div>
+      <div>Albums for {user.name}</div>
+      <div>{content}</div>
+    </div>
   )
 }
 
